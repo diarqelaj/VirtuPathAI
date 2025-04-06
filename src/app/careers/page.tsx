3a1343c127a8d1a264533c68c70d48f7846d557a
Dijar <diarqelaj15@gmail.com>
2025-04-06 06:16:04 +0200

    Styling updates in careers>page.tsx

--- src/app/careers/page.tsx
+++ src/app/careers/page.tsx
@@ -5,13 +5,14 @@ import { careers, navItems } from "@/data";
 import Image from "next/image";
 import Footer from "@/components/Footer";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
+import '../globals.css';
 
 const CareersPage = () => {
   return (
     <div className="relative bg-black-100 text-white flex flex-col min-h-screen">
   <FloatingNav navItems={navItems} />
 
-  <div className="pt-55 px-4 md:px-6 flex-grow"> {/* Increased top padding */}
+  <div className="pt-50 px-4 md:px-6 flex-grow"> {/* Increased top padding */}
     <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
     <Spotlight className="h-[80vh] w-[50vw] top-10 left-full" fill="purple" />
     <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
@@ -26,7 +27,7 @@ const CareersPage = () => {
         {careers.map((program) => (
           <div
             key={program.id}
-            className="bg-[#1a1b2f] p-6 rounded-xl shadow-lg border border-gray-800 hover:scale-[1.03] transition-transform duration-200"
+            className="bg-[#0c0c16] p-6 rounded-xl shadow-lg border border-gray-800 hover:scale-[1.03] transition-transform duration-200"
           >
             <div className="relative w-full h-40 mb-4">
               <Image
@@ -39,7 +40,7 @@ const CareersPage = () => {
             <h3 className="text-2xl font-semibold mb-2">{program.title}</h3>
             <p className="text-sm text-gray-400 mb-1">{program.location}</p>
             <p className="text-gray-300 text-sm mb-4">{program.description}</p>
-            <p className="text-blue-400 font-semibold text-lg">
+            <p className="text-deeppurple-100 font-semibold text-lg">
               🎓 Training Cost: {program.price}
             </p>
           </div>
