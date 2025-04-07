5903621788ff6a9a8622f08e1df799d6a7b7ffe3
Dijar <diarqelaj15@gmail.com>
2025-04-08 01:24:27 +0200

    removed unused tsx files

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,7 @@
-// eslint.config.js
-import typescriptEslintParser from "@typescript-eslint/parser";
-import typescriptEslintPlugin from "@typescript-eslint/eslint-plugin";
-
-export default [
-  {
-    files: ["**/*.ts", "**/*.tsx"], // Specify the files to lint
-    languageOptions: {
-      parser: typescriptEslintParser,
-    },
-    plugins: {
-      "@typescript-eslint": typescriptEslintPlugin,
-    },
-    rules: {
-      ...typescriptEslintPlugin.configs.recommended.rules, // Spread the recommended rules
-    },
-  },
-];
+module.exports = {
+  "parser": "@typescript-eslint/parser",
+  "plugins": ["@typescript-eslint"],
+  "extends": [
+    "plugin:@typescript-eslint/recommended"
+  ]
+};
