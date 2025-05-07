a8c20669423fd4d687b7f5548ce72d4ab67d0eb2
Dijar <diarqelaj15@gmail.com>
2025-05-08 00:45:02 +0200

    Added backend support

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,16 +1,10 @@
-// api.ts
 import axios from 'axios';
-import https from 'https';
-
-const agent = new https.Agent({ rejectUnauthorized: false });
 
 const api = axios.create({
-  baseURL: 'https://localhost:7072/api',
-  httpsAgent: agent,
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
 });
 
-// ✅ Attach timezone automatically to every request
 api.interceptors.request.use((config) => {
   const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   config.headers['X-Timezone'] = timezone;
